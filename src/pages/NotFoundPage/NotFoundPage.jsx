34c66cdbd81ea46fecc43557702e0d131a02a975
Артем Томащук <tomashhukartem@gmail.com>
2024-11-20 07:39:05 +0200

    change code

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,4 +1,4 @@
-import Error from "../../components/Error/Error";
+import Error from "../../components/ErrorMessage/ErrorMessage";
 
 const NotFoundPage = () => {
   return <Error status={"404"} message={"Oh no! Page not found:("} />;
